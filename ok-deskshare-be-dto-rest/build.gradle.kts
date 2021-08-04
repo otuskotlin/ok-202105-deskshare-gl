802d17e1f87e05a6ce2c0c099e7e632727ac38a4
grischaLib <grigori.liberchuk@fashion-digital.de>
2021-08-04 12:31:59 +0200

    m3: tests

--- ok-deskshare-be-dto-rest/build.gradle.kts
+++ ok-deskshare-be-dto-rest/build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+
+    testImplementation(kotlin("test-junit"))
 }
 
 tasks {
