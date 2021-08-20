f265fdd31620d4b49e35da00d415e48b26d0e203
grischaLib <grigori.liberchuk@fashion-digital.de>
2021-08-20 17:43:52 +0200

    ktor init

---
+++ ok-deskshare-app-ktor/build.gradle.kts
@@ -0,0 +1,29 @@
+val ktorVersion: String by project
+val logbackVersion: String by project
+
+fun DependencyHandler.ktor(module: String, version: String? = ktorVersion): Any =
+    "io.ktor:ktor-$module:$version"
+
+plugins {
+    application
+    kotlin("jvm")
+}
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+
+    implementation(ktor("server-core"))
+    implementation(ktor("server-netty"))
+    implementation(ktor("jackson"))
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+
+    testImplementation(kotlin("test-junit"))
+    testImplementation(ktor("server-test-host"))
+}
